74f8e123fd04faad1168d79c456efa94879908f3
Velman Harefa <velmanh24@gmail.com>
2024-12-27 10:21:23 +0700

    app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 from ultralytics import YOLO
 import time
 
-MODEL_PATH = "best_modelSDG100.pt"  # Path ke model YOLOv11 Anda
+MODEL_PATH = "best_modelSDG100.pt" 
 
 # Load model
 model = YOLO(MODEL_PATH)
@@ -15,7 +15,6 @@ st.write("Upload gambar untuk mendeteksi dan mengklasifikasi buah.")
 # Warna hasil prediksi sama dengan warna area upload
 RESULT_BOX_STYLE = "color: white; background-color: #4CAF50; padding: 10px; border-radius: 5px;"
 
-# Upload gambar
 uploaded_file = st.file_uploader("Upload gambar", type=["jpg", "png", "jpeg"])
 
 # Simpan state gambar yang diunggah
@@ -27,26 +26,26 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Gambar yang diunggah")
 
-    # Tangani gambar dengan transparansi (RGBA)
+    # Gambar dengan transparansi (RGBA)
     if image.mode == "RGBA":
         image = image.convert("RGB")  # Konversi ke RGB
 
     # Simpan gambar ke state untuk prediksi
     st.session_state['image'] = image
 
-# Tombol prediksi
+# Button prediksi
 if st.session_state['image'] is not None and st.button("Mulai Prediksi"):
     with st.spinner("Memproses gambar, mohon tunggu..."):
-        time.sleep(2)  # Simulasi loading untuk user experience
+        time.sleep(2)  
 
-        # Simpan gambar sementara
+        # Menyimpan gambar untuk di prediksi 
         temp_image_path = "temp_uploaded_image.jpg"
         st.session_state['image'].save(temp_image_path, format="JPEG")
 
         # Prediksi dengan YOLOv11
         results = model(temp_image_path)
 
-        # Tampilkan hasil deteksi
+        # Menampilkan hasil deteksi
         st.write("Hasil Deteksi:")
         original_image = st.session_state['image'].copy()
         draw = ImageDraw.Draw(original_image)
@@ -57,14 +56,14 @@ if st.session_state['image'] is not None and st.button("Mulai Prediksi"):
                 label = result.names[int(box.cls)]
                 confidence = float(box.conf)
 
-                # Tambahkan bounding box ke gambar
+                # Bounding box ke gambar
                 draw.rectangle([x1, y1, x2, y2], outline="red", width=3)
 
-                # Tambahkan label dan confidence
+                # Menambahkan label dan confidence
                 text = f"{label} {confidence:.2%}"
                 draw.text((x1, y1 - 10), text, fill="red")
 
                 st.markdown(f"<div style='{RESULT_BOX_STYLE}'>Label: {label}, Confidence: {confidence:.2%}</div>", unsafe_allow_html=True)
 
-        # Tampilkan gambar dengan bounding box
+        # Menampilkan gambar dengan bounding box
         st.image(original_image, caption="Gambar dengan Bounding Box")
